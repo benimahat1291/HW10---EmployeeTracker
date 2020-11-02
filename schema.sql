db638ed6dcc9aadd9772e060e3852c0bb50abf2a
Beni Mahat <benimahat6565@gmail.com>
2020-11-01 18:52:40 -0700

    built tables

--- schema.sql
+++ schema.sql
@@ -3,17 +3,16 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id integer auto_increment not null;
-    name varchar(30) not null;
-    primary key(id);
+    id integer auto_increment not null,
+    name varchar(30) not null,
+    primary key(id)
 );
 
 CREATE TABLE role(
-    id integer auto_increment not null;
-    title varchar(30) not null;
-    salary decimal not null;
-    department_id integer not null;
-    constraint fk_department_id foreign key (department_id) references department(id),
+    id integer auto_increment not null,
+    title varchar(30) not null,
+    salary decimal not null,
+    department_id integer not null,
     primary key(id)
 );
 
@@ -22,8 +21,6 @@ CREATE TABLE employees(
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id integer not null,
-    constraint fk_role_id FOREIGN KEY (manager_id) REFRENCES employees(id);
+    manager_id integer,
+    primary key(id)
 );
-select = from employee;
-select = from role;
-select = from department;
