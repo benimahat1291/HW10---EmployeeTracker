b2c4e49e298301eb78c178fc0a0e3e791033ed13
Beni Mahat <benimahat6565@gmail.com>
2020-11-01 19:27:27 -0700

    added columes to tables

--- schema.sql
+++ schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE department(
     primary key(id)
 );
 
-CREATE TABLE role(
+
+CREATE TABLE roles(
     id integer auto_increment not null,
     title varchar(30) not null,
     salary decimal not null,
@@ -20,7 +21,49 @@ CREATE TABLE employees(
     id integer auto_increment not null,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
-    role_id integer not null,
+    roles_id integer not null,
     manager_id integer,
     primary key(id)
 );
+
+
+INSERT into department (name)
+VALUES ("Manager");
+INSERT into department (name)
+VALUES ("Sales");
+INSERT into department (name)
+VALUES ("Legal");
+INSERT into department (name)
+VALUES ("Engineering");
+INSERT into department (name)
+VALUES ("Finance");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Manager", 65000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 45000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Salesperson", 35000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Lead Engineer", 43000, 4);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Accountant", 50000, 5);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Legal", 50000, 3);
+
+
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Timmy","Stevens", 1);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Jonny","Grayson", 2);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Jack","Sparrow", 3);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Harry","Potter", 4);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Jimmy","Fallon", 5);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Bruce","Wayne", 6);
+INSERT INTO employees (first_name, last_name, roles_id)
+VALUES ("Peter","Parker", 7);
+
